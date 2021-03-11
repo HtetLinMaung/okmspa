a15f4b8cf1cb48024d7a23f1019dc393e5764c47
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2021-03-11 16:00:02 +0630

    update

--- src/app/registration/customer-registration/customer-registration.component.ts
+++ src/app/registration/customer-registration/customer-registration.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-customer-registration',
   templateUrl: './customer-registration.component.html',
-  styleUrls: ['./customer-registration.component.css']
+  styleUrls: ['./customer-registration.component.css'],
 })
 export class CustomerRegistrationComponent implements OnInit {
+  selectedFiles = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  chooseFile() {
+    document.getElementById('fileInput')?.click();
   }
 
+  onFileChoosen(e) {
+    let i = 1;
+    for (const file of e.target.files) {
+      this.selectedFiles.push({ file, key: i++ });
+    }
+    e.target.value = '';
+  }
+
+  deleteFile(key: number) {
+    this.selectedFiles = this.selectedFiles.filter((v) => v.key != key);
+  }
 }
