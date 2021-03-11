fd45ad10730181d72696c2650a4fac0625edd85e
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro.local>
2021-03-11 17:20:41 +0630

    update

--- src/app/registration/international-remittance/international-remittance.component.ts
+++ src/app/registration/international-remittance/international-remittance.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-international-remittance',
   templateUrl: './international-remittance.component.html',
-  styleUrls: ['./international-remittance.component.css']
+  styleUrls: ['./international-remittance.component.css'],
 })
 export class InternationalRemittanceComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
